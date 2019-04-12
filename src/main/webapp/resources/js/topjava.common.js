44ca94df2a3d23b2d269c0f090197605707577e4
Cavle <plabdnet01@gmail.com>
2019-04-12 15:49:29 +0300

    meal rest controller impl

--- src/main/webapp/resources/js/topjava.common.js
+++ src/main/webapp/resources/js/topjava.common.js
@@ -1,5 +1,6 @@
 function makeEditable(ctx) {
     context = ctx;
+    filterForm = $('#filterForm');
     form = $('#detailsForm');
     $(".delete").click(function () {
         if (confirm('Are you sure?')) {
@@ -36,6 +37,12 @@ function updateTable() {
     });
 }
 
+function resetFilter() {
+    $(".reset").closest('form').find("input[type=date], textarea").val("");
+    $(".reset").closest('form').find("input[type=time], textarea").val("");
+    updateTable();
+}
+
 function save() {
     $.ajax({
         type: "POST",
@@ -43,10 +50,25 @@ function save() {
         data: form.serialize()
     }).done(function () {
         $("#editRow").modal("hide");
-        updateTable();
+        if ($("#startDate").val() != null || $("#endDate").val() != null)
+            updateTableFiltered();
+        else
+            updateTable();
         successNoty("Saved");
     });
 }
+
+function updateTableFiltered() {
+    $.ajax({
+        type: "GET",
+        url: context.ajaxUrlFilter,
+        data: filterForm.serialize()
+    }).done(function (data) {
+        context.datatableApi.clear().rows.add(data).draw();
+    })
+}
+
+var failedNote;
 
 function closeNoty() {
     if (failedNote) {
@@ -61,8 +83,21 @@ function successNoty(text) {
         text: "<span class='fa fa-lg fa-check'></span> &nbsp;" + text,
         type: 'success',
         layout: "bottomRight",
-        timeout: 1000
+        timeout: 2000
     }).show();
+}
+
+function enable(chkbox) {
+    var enabled = chkbox.is(":checked");
+    chkbox.parent().parent().css("text-decoration", enabled ? "none" : "line-through");
+    $.ajax({
+        url: ajaxUrl + chkbox.attr('id1'),
+        type: 'POST',
+        data: 'enabled=' + enabled,
+        success: function () {
+            successNoty(enabled ? 'Enabled' : 'Disabled');
+        }
+    })
 }
 
 function failNoty(jqXHR) {
@@ -73,3 +108,15 @@ function failNoty(jqXHR) {
         layout: "bottomRight"
     }).show();
 }
+
+$(".userState").onchange(function () {
+    alert("hihi");
+});
+
+$(".userState").change(function () {
+    alert("hehe");
+    if ($(this).is(':checked')) {
+        alert('checked');
+    } else
+        alert('unchecked')
+});
---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,37 @@
+// $(document).ready(function () {
+$(function () {
+    makeEditable({
+            ajaxUrl: "ajax/profile/meals/",
+            ajaxUrlFilter: "ajax/profile/meals/filtered",
+            datatableApi: $("#datatablemeals").DataTable({
+                "paging": false,
+                "info": true,
+                "columns": [
+                    {
+                        "data": "dateTime"
+                    },
+                    {
+                        "data": "description"
+                    },
+                    {
+                        "data": "calories"
+                    },
+                    {
+                        "defaultContent": "Edit",
+                        "orderable": false
+                    },
+                    {
+                        "defaultContent": "Delete",
+                        "orderable": false
+                    }
+                ],
+                "order": [
+                    [
+                        0,
+                        "asc"
+                    ]
+                ]
+            })
+        }
+    );
+});
